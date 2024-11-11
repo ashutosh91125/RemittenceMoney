31aa38de6d5588c17ad96ef7729c61c90a571c0c
HP <HP@DESKTOP-PJ2LOMT>
2024-11-11 17:46:03 +0530

    gender changeds

--- src/main/java/com/llm/model/Gender.java
+++ src/main/java/com/llm/model/Gender.java
@@ -1,21 +1,4 @@
 package com.llm.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Table(name = "Gender")
-@Entity
-public class Gender {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer gId;
-	private String description;
-
+public enum Gender {
 }

9bd389f7d03359f1403ce18347edaabfbcf6da63
Ashu <vinay.sharma@9yardstechnology.com>
2024-11-26 13:01:14 +0530

    redirect changed

--- src/main/java/com/llm/Service/CustomerService.java
+++ src/main/java/com/llm/Service/CustomerService.java
@@ -140,10 +140,16 @@ public class CustomerService {
 			Map<String, Object> idData = new HashMap<>();
 
 			if (customer.getResidentTypeId() == 101) {
-//                customer.setIssuedOn((LocalDate.now()).toString());
-//                customer.setDateOfExpiry((LocalDate.now().plusYears(2)).toString());
-				customer.setIssuedOn("2024-11-26");
-				customer.setDateOfExpiry("2026-11-26");
+                customer.setIssuedOn((LocalDate.now(mytZone)).toString());
+                customer.setDateOfExpiry((LocalDate.now(mytZone).plusYears(2)).toString());
+//				customer.setIssuedOn("2024-11-26");
+//				customer.setDateOfExpiry("2026-11-26");
+
+				String issued = customer.getIssuedOn().trim();
+				issued = issued.substring(0,10);
+
+				String doe = customer.getDateOfExpiry().trim();
+				doe = doe.substring(0,10);
 
 				customer.setIdType(28);
 				customer.setIssuedCountry("MY");
@@ -160,11 +166,11 @@ public class CustomerService {
 				idData.put("issued_by", customer.getIssuedBy());
 				idData.put("issued_at", customer.getIssuedAt());
 
-				customerData.put("issued_on", customer.getIssuedOn());
-				customerData.put("date_of_expiry", customer.getDateOfExpiry());
+//				customerData.put("issued_on", customer.getIssuedOn());
+//				customerData.put("date_of_expiry", customer.getDateOfExpiry());
 
-				idData.put("issued_on", "2024-11-26");
-				idData.put("date_of_expiry", "2026-11-26");
+				idData.put("issued_on", issued);
+				idData.put("date_of_expiry", doe);
 
 				idData.put("active_status", customer.getActiveStatus());
 
